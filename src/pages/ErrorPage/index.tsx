27d40f142c013f74eef2891282a4b592fb109352
elpwc/wniko <elpwc@hotmail.com>
2022-12-02 08:07:53 +0800

    404

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import './index.css';
-
+import image_lonely from '../../resources/lonely.png';
 interface P {}
 
 export default (props: P) => {
@@ -15,7 +16,14 @@ export default (props: P) => {
     document.title = 'Kikaku - 404';
   }, []);
 
-  return <>
-    404
-  </>;
+  return (
+    <div className="w-full h-full flex justify-center">
+      <div className="bg-white rounded-lg shadow-lg w-2/4 h-fit px-10 py-10 flex flex-col items-center">
+        <img src={image_lonely} className="h-40" />
+        <Link to="/" className="btn-blue">
+          返回主页
+        </Link>
+      </div>
+    </div>
+  );
 };
